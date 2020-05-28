7ab4f6a4bd56fbe196caa631d86068f7547e1937
贝克街的流浪猫 <517375685@qq.com>
2020-05-28 16:01:46 +0800

    Add algorithm

---
+++ src/main/java/bbm/leetcode/Question11.java
@@ -0,0 +1,37 @@
+package bbm.leetcode;
+
+/**
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。
+ *
+ * 示例：
+ *
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/container-with-most-water
+ *
+ * @author bbm
+ * @date 2020/5/28
+ */
+public class Question11 {
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int left = 0, right = height.length - 1; left < right;) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Question11().maxArea(new int[] {2, 3, 4, 5, 18, 17, 6}));
+    }
+}
---
+++ src/main/java/bbm/leetcode/Question384.java
@@ -0,0 +1,71 @@
+package bbm.leetcode;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static bbm.leetcode.Utils.print;
+
+/**
+ * 打乱一个没有重复元素的数组。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * // 以数字集合 1, 2 和 3 初始化数组。
+ * int[] nums = {1,2,3};
+ * Solution solution = new Solution(nums);
+ *
+ * // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。
+ * solution.shuffle();
+ *
+ * // 重设数组到它的初始状态[1,2,3]。
+ * solution.reset();
+ *
+ * // 随机返回数组[1,2,3]打乱后的结果。
+ * solution.shuffle();
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shuffle-an-array
+ *
+ * @author bbm
+ * @date 2020/5/28
+ */
+public class Question384 {
+    static class Solution {
+
+        private int[] original;
+        Random random = new Random(System.currentTimeMillis());
+
+        public Solution(int[] nums) {
+            this.original = nums;
+        }
+
+        /** Resets the array to its original configuration and return it. */
+        public int[] reset() {
+            return original;
+        }
+
+        /** Returns a random shuffling of the array. */
+        public int[] shuffle() {
+            int[] result = Arrays.copyOf(original, original.length);
+            for (int i = 0; i < result.length; i++) {
+                int exchangeIndex = i + random.nextInt(result.length - i);
+                int temp = result[exchangeIndex];
+                result[exchangeIndex] = result[i];
+                result[i] = temp;
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution(new int[]{-9, 10, 100, 20});
+        print(solution.reset());
+        print(solution.shuffle());
+        print(solution.shuffle());
+        print(solution.shuffle());
+        print(solution.shuffle());
+        print(solution.shuffle());
+    }
+}
